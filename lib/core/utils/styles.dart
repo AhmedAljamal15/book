a4228c8586fe661037a3cbe914ff6dc02e3be58d
AhmedAljamal <gad59125@gmail.com>
2025-04-27 15:51:07 +0300

    add class styles & create text best seller

---
+++ lib/core/utils/styles.dart
@@ -0,0 +1,26 @@
+
+import 'package:flutter/material.dart';
+
+
+abstract class Styles {
+  static const semiBold = TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.w600,
+    color: Colors.white,
+  );
+  
+}
+
+
+
+
+// class AppColors {
+//   static const Color kPrimaryColor = Color(0xFF4C68FF);
+//   static const Color kScaffoldColor = Color(0xffF7F7F7);
+//   static const Color kBordersideColor = Color(0xffE1E1E1);
+//   static const Color kWhiteColor = Color(0xFFffffff);
+//   static const Color kBlackColor = Color(0xFF17162B);
+//   static const Color kGreyColor = Colors.grey;
+
+  
+// }
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:book/core/utils/styles.dart';
 import 'package:book/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:book/features/home/presentation/views/widgets/featured_books_list_view.dart';
 import 'package:flutter/material.dart';
@@ -7,16 +8,24 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [CustomAppBar(), FeaturedBooksListView(),],);
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children:[
+        CustomAppBar(), 
+        FeaturedBooksListView(),
+        SizedBox(
+          height: 50,
+        ),
+        Text(
+          "Best Seller",
+          style:  Styles.semiBold,
+        ),
+      ],
+      ),
+    );
   }
 }
 
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
