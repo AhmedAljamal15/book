8243436c9b480be7d2e1d87bf2291f5b6a8dd0d6
AhmedAljamal <gad59125@gmail.com>
2025-04-29 14:17:54 +0300

    create repos

--- lib/features/home/presentation/views/widgets/best_seller_list_item.dart
+++ lib/features/home/presentation/views/widgets/best_seller_list_item.dart
@@ -1,7 +1,4 @@
-import 'package:book/constants.dart';
-import 'package:book/core/utils/assets.dart';
-import 'package:book/core/utils/styles.dart';
-import 'package:book/features/home/presentation/views/widgets/book_rateing.dart';
+
 import 'package:book/features/home/presentation/views/widgets/custom_best_seller_list_view.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
